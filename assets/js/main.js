f1e9aae060bf17fac0c9b00922dcbfb083192d30
Akash <akkusahu559@gmail.com>
2021-09-01 08:28:07 +0530

    swiper functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,6 +98,21 @@ modalCloses.forEach((modalCLose) => {
 
 
 /* ================================== PORTFOLIO SWIPER ======================================== */
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 
 /* ================================== TESTIMONAL ======================================== */
